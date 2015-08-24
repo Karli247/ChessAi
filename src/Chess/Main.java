5d7a6128303c54770e0ea113444df4321a2b39ec
Karli247 <Karli247@users.noreply.github.com>
2015-08-24 16:49:59 +0200

    Update Main.java
    Frame

--- src/Chess/Main.java
+++ src/Chess/Main.java
@@ -8,14 +8,14 @@ import javax.swing.*;
 public class Main {
   public static void main(String[] args)
   {
-    System.out.println("ChessAI");
-    Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-    JFrame frame = new JFrame();
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(d);
-		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		frame.setUndecorated(true);
-		frame.setVisible(true);
+    	System.out.println("ChessAI");
+    	Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+    	JFrame frame = new JFrame();
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setSize(d);
+	frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+	frame.setUndecorated(true);
+	frame.setVisible(true);
     
   }
 
